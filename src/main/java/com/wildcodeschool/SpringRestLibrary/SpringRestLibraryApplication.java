c8a2ba444a408f79d94c7ec443eaffc5d36a2d92
Rémy Trohel <remy.trohel@gmail.com>
2019-07-29 12:54:01 +0200

    quest completed

---
+++ src/main/java/com/wildcodeschool/SpringRestLibrary/SpringRestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.SpringRestLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestLibraryApplication.class, args);
+	}
+
+}
